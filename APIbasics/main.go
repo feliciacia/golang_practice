1ef6415ea6f7f0385449b6225b64f00fc96c5add
羅翡幸 <feliciaalo08@gmail.com>
2023-06-12 13:15:58 +0800

    create_methods

---
+++ APIbasics/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type movie struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Director string `json:"director"`
+	Price    string `json:"price"`
+}
+
+var movies = []movie{
+	{ID: "1", Title: "The Dark Knight", Director: "Cristopher Nolan", Price: "5.99"},
+	{ID: "2", Title: "Tommy Boy", Director: "Peter Segal", Price: "2.99"},
+	{ID: "3", Title: "The Shawshank Redemption", Director: "Frank Darabont", Price: "7.99"},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/movie", getMovies)
+	router.GET("/movie/:id", getMovieByID)
+	router.POST("/movie", createMovie)
+	router.PATCH("/movie/:id", updateMoviePrice)
+	router.DELETE("/movie/:id", deleteMovie)
+
+	err := router.Run("localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getMovies(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, movies)
+}
+
+func getMovieByID(c *gin.Context) {
+	id := c.Param("id")
+	var index int
+
+	for k, v := range movies {
+		if id == v.ID {
+			index = k
+		}
+	}
+	c.IndentedJSON(http.StatusOK, movies[index])
+}
